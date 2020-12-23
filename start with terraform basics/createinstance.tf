59216e8926d57aa9ca4ff07c3b124ff544e6c211
jeevan828 <jeevan828@hotmail.com>
2020-12-22 19:56:08 -0500

    ssss

--- start with terraform basics/createinstance.tf
+++ start with terraform basics/createinstance.tf
@@ -1,11 +1,11 @@
-provider "AWS" {
+provider "aws" {
   access_key = "AKIA3PDMQJKMGTLX7YUY"
   secret_key = "secret_key"
   region     = "us-east-1"
 }
 
-resorce "AWS_instance" "Myfirstinstance" {
+resource "aws_instance" "Myfirstinstance" {
   ami           = "ami-099e921e69356cf89"
   instance_type = "t2.micro"
-
 }
+
