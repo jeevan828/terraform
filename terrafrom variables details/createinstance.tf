6554d1b887aeff83c081f02e92e6ada0cca08c0c
jeevan828 <jeevan828@hotmail.com>
2020-12-23 16:05:12 -0500

    adding files

---
+++ terrafrom variables details/createinstance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "Myfirstinstance" {
+  ami           = "ami-099e921e69356cf89"
+  instance_type = "t2.micro"
+
+  tags = {
+
+    Name = "demoinstatnce"
+  }
+}
---
+++ terrafrom variables details/variables.tf
@@ -0,0 +1,7 @@
+variables "AWS_ACCESS_KEY" {}
+
+variables "AWS_SECRET_KEY" {}
+
+variables "AWS_REGION" {
+default = ""us-east-1"
+}
