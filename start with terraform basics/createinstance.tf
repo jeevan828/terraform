7130ae27e4bba263091138a96f4a705a214ea7f1
jeevan828 <jeevan828@hotmail.com>
2020-12-23 13:35:44 -0500

    create provider file

--- start with terraform basics/createinstance.tf
+++ start with terraform basics/createinstance.tf
@@ -1,11 +1,4 @@
-provider "aws" {
-  access_key = "AKIA3PDMQJKMGTLX7YUY"
-  secret_key = "secret_key"
-  region     = "us-east-1"
-}
-
 resource "aws_instance" "Myfirstinstance" {
   ami           = "ami-099e921e69356cf89"
   instance_type = "t2.micro"
 }
-
---
+++ start with terraform basics/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIA3PDMQJKMGTLX7YUY"
+  secret_key = "secret_key"
+  region     = "us-east-1"
+}
