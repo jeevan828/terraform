a199d476257392c453b225afcce46dab3f255f87
jeevan828 <jeevan828@hotmail.com>
2020-12-23 14:42:19 -0500

    create provider file

--- start with terraform basics/createinstance.tf
+++ start with terraform basics/createinstance.tf
@@ -2,4 +2,9 @@ resource "aws_instance" "Myfirstinstance" {
   count         = 3
   ami           = "ami-099e921e69356cf89"
   instance_type = "t2.micro"
+
+  tags = {
+
+    Name = "threedemoinstatnce"
+  }
 }
